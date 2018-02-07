d9e00241eeb124ea1e2713fcf19ca5090d4d3449
Gerrit Jamerson <gjamerson@renown.org>
2018-02-06 21:23:31 -0800

    Minor changes and added some skeleton classes

---
+++ SFMLEngine/IGameService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SFMLEngine {
+	public interface IGameService {
+		void initializeService(GameContext context);
+		void updateService(GameContext context);
+		void drawService(GameContext context);
+	}
+}
