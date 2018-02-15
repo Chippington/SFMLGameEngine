207b871a05a0acab38f5425b55b7c494ce07face
Chippington <scythix@hotmail.com>
2018-02-15 13:50:03 -0800

    Added some debug implementations for tilesets

---
+++ SFMLEngine/Utilities/Tilesets/Default/Debug/DebugGridTilesetAdapter.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SFMLEngine.Utilities.Grids;
+
+namespace SFMLEngine.Utilities.Tilesets.Default.Debug {
+	public class DebugGridTilesetAdapter<T> : ObjectBase, ITilesetAdapter<T> {
+		private IGridBase<T> grid;
+
+		public void onInitialize(GameContext context) {
+			log("Initialized");
+		}
+
+		public IGridBase<T> getGrid() {
+			return grid;
+		}
+
+		public void setGrid(IGridBase<T> grid) {
+			this.grid = grid;
+		}
+
+		public void onTileCreated(IGridNode<T> tile) {
+			log("Tile created at dim: " + tile.getDim().ToString());
+		}
+
+		public void onTileDeleted(IGridNode<T> tile) {
+			log("Tile removed at dim: " + tile.getDim().ToString());
+		}
+
+		public void onDispose(GameContext context) {
+			log("Disposed");
+		}
+	}
+}
---
+++ SFMLEngine/Utilities/Tilesets/Default/Debug/DebugGridTilesetRenderer.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SFMLEngine.Utilities.Grids;
+
+namespace SFMLEngine.Utilities.Tilesets.Default.Debug {
+	public class DebugGridTilesetRenderer<T> : ITilesetRenderer<T> {
+		public void onInitialize(GameContext context) {
+			throw new NotImplementedException();
+		}
+
+		public void onDraw(GameContext context) {
+			throw new NotImplementedException();
+		}
+
+		public IGridBase<T> getGrid() {
+			throw new NotImplementedException();
+		}
+
+		public void setGrid(IGridBase<T> grid) {
+			throw new NotImplementedException();
+		}
+
+		public void onDispose(GameContext context) {
+			throw new NotImplementedException();
+		}
+	}
+}
