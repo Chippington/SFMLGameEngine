a49a7d13264f8fb78908744086acdba4f8be14cf
Chippington <scythix@hotmail.com>
2018-03-05 12:57:38 -0800

    Implemented debug logging interface, cleaned up debug logging

---
+++ SFMLEngine/Debug/LogBlock.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SFMLEngine.Debug {
+	public class TimedLogBlock : LogBlock {
+		Func<TimeSpan, string> func;
+		Stopwatch sw;
+
+		public TimedLogBlock(Func<TimeSpan, string> func) : base("") {
+			this.func = func;
+			sw = new Stopwatch();
+			sw.Start();
+		}
+
+		public override void Dispose() {
+			sw.Stop();
+			string str = "";
+			if (func != null)
+				str = func.Invoke(sw.Elapsed);
+
+			Console.WriteLine(str);
+		}
+	}
+
+	public class LogBlock : IDisposable {
+		private string str;
+		Func<string> strFunc;
+
+		public LogBlock(string str) {
+			this.str = str;
+		}
+
+		public LogBlock(Func<string> func) {
+			strFunc = func;
+		}
+
+		public virtual void Dispose() {
+			if (strFunc != null)
+				str = strFunc.Invoke();
+
+			Console.WriteLine(str);
+		}
+	}
+}
